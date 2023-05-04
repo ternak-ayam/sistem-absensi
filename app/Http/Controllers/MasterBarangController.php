a6f6e46bfa2bb4dda2f0c2c82752c72b3d6a0d06
xarthaa <manager.arthachan@gmail.com>
2023-05-04 23:14:24 +0800

    only barang masuk in master

--- app/Http/Controllers/MasterBarangController.php
+++ app/Http/Controllers/MasterBarangController.php
@@ -9,7 +9,10 @@ class MasterBarangController extends Controller
 {
     public function index()
     {
-        $products = Product::where('code', 'like', '%'.\request()->get('search').'%')->orderby('id', 'DESC')->paginate(10);
+        $products = Product::where('type', Product::MASUK)
+            ->where('code', 'like', '%'.\request()->get('search').'%')
+            ->orderby('id', 'DESC')
+            ->paginate(10);
 
         return view('admin.pages.barang.master.index', [
             'products' => $products
