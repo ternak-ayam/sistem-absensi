0455bb2c85fe1b2290c19e75d65de8a6690006d4
xarthaa <manager.arthachan@gmail.com>
2023-09-10 11:05:00 +0800

    modify sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -15,63 +15,14 @@
                 <i class="fas fa-home"></i> <span>Dashboard</span>
             </a>
         </li>
-        @can('admin')
-            <li {{ is_nav_active('user') }}>
-                <a class="nav-link" href="{{ route('admin.user.index') }}">
-                    <i class="fas fa-users"></i> <span>Users</span>
-                </a>
-            </li>
-        @endcan
-        <li class="dropdown {{ is_drop_active('barang') }}">
-            <a href="#" class="nav-link has-dropdown">
-                <i class="fas fa-box"></i> <span>Barang</span>
+        <li {{ is_nav_active('user') }}>
+            <a class="nav-link" href="{{ route('admin.user.index') }}">
+                <i class="fas fa-users"></i> <span>Pegawai</span>
             </a>
-            <ul class="dropdown-menu">
-                @canany(['admin', 'pegawai', 'owner'])
-                    @canany(['admin', 'pegawai'])
-                        <li {{ is_nav_active('master') }}>
-                            <a class="nav-link" href="{{ route('admin.barang.master.index') }}">
-                                <i class="far fa-circle"></i> Master Barang
-                            </a>
-                        </li>
-                    @endcanany
-                    @can('admin')
-                        <li {{ is_nav_active('list') }}>
-                            <a class="nav-link" href="{{ route('admin.barang.list.index') }}">
-                                <i class="far fa-circle"></i> List Barang
-                            </a>
-                        </li>
-                        <li {{ is_nav_active('masuk') }}>
-                            <a class="nav-link" href="{{ route('admin.barang.masuk.index') }}">
-                                <i class="far fa-circle"></i> Barang Masuk
-                            </a>
-                        </li>
-                        <li {{ is_nav_active('keluar') }}>
-                            <a class="nav-link" href="{{ route('admin.barang.keluar.index') }}">
-                                <i class="far fa-circle"></i> Barang Keluar
-                            </a>
-                        </li>
-                    @endcan
-                    @canany(['admin', 'pegawai'])
-                        <li {{ is_nav_active('kelola') }}>
-                            <a class="nav-link" href="{{ route('admin.barang.index') }}">
-                                <i class="far fa-circle"></i> Kelola Barang
-                            </a>
-                        </li>
-                    @endcanany
-                    @can('admin')
-                        <li {{ is_nav_active('retur') }}>
-                            <a class="nav-link" href="{{ route('admin.barang.return.index') }}">
-                                <i class="far fa-circle"></i> Retur Barang
-                            </a>
-                        </li>
-                    @endcan
-                @endcanany
-            </ul>
         </li>
-        <li {{ is_nav_active('laporan') }}>
-            <a class="nav-link" href="{{ route('admin.laporan.index') }}">
-                <i class="fas fa-folder-open"></i> Laporan Barang
+        <li {{ is_nav_active('user') }}>
+            <a class="nav-link" href="{{ route('admin.user.index') }}">
+                <i class="fas fa-paste"></i> <span>Absensi Pegawai</span>
             </a>
         </li>
     </ul>
